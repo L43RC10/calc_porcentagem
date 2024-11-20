05d357571d4e04995ad5813d785fad3a30120e22
Laercio Pereira <laercio3714@gmail.com>
2024-11-20 16:36:32 -0300

    tarefa finalizada

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ btnCalculate.addEventListener('click', (e) => {
         return;
     }
 
-    const resultado = valor * (porcentagem / 100);
+    const resultado = valor * (porcentagem/100);
     const total = valor *(1 + porcentagem/100);
 
 
-    // ou se fosse o caso de alterar uma class: resposta.classList.remove('nome_clas)
+    // ou se fosse o caso de alterar uma class: resposta.classList.remove('nome_class)
     resposta.style.display = 'block';
 
     resposta.textContent = `${porcentagem}% de ${valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} é ${resultado.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}. A soma do valor mais a porcentagem é ${total.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
@@ -27,6 +27,8 @@ btnCalculate.addEventListener('click', (e) => {
     // resposta.textContent = `${porcentagem}% de R$${valor.toFixed(2).replace('.',',')} é R$${resultado.toFixed(2).replace('.',',')}. A soma do valor mais a porcentagem é R$${total.toFixed(2).replace('.',',')}`;
 
 });
+
+
 
 btnLimpar.addEventListener('click', (e) => {
     e.preventDefault();
