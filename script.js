b22a274d3c63db4fd066364f753f71218940f6e5
Laercio Pereira <laercio3714@gmail.com>
2024-11-20 14:27:20 -0300

    arrumando o style da pagina com CSS puro

---
+++ script.js
@@ -0,0 +1,4 @@
+const valor = document.getElementById("valor");
+const porcentagem = document.getElementById("porcentagem");
+const btn_calculate = document.getElementById("btn_calculate");
+const resposta = document.getElementById("resposta");
