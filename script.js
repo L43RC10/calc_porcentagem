79e1f2270dcd0a7a92be5dab91bbb804808e70e4
Laercio Pereira <laercio3714@gmail.com>
2024-11-20 16:08:57 -0300

    finalizado script do JS

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const elValor = document.getElementById("valor");
 const elPorcentagem = document.getElementById("porcentagem");
 const btnCalculate = document.getElementById("btn_calculate");
+const btnLimpar = document.getElementById("btn_limpar");
 const resposta = document.getElementById("resposta");
 
-btnCalculate.addEventListener("click", (e) => {
+btnCalculate.addEventListener('click', (e) => {
     e.preventDefault();
     const valor = Number(elValor.value);
     const porcentagem = Number(elPorcentagem.value);
 
     if(valor == "" || isNaN(valor) || (porcentagem == "" || isNaN(porcentagem))){
         alert('Preencha os campos com números...');
+        elValor.focus();
         return;
     }
 
@@ -20,8 +22,18 @@ btnCalculate.addEventListener("click", (e) => {
     // ou se fosse o caso de alterar uma class: resposta.classList.remove('nome_clas)
     resposta.style.display = 'block';
 
-    resposta.textContent = `${porcentagem}% de ${valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} é R$${resultado.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}. A soma do valor mais a porcentagem é ${total.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
+    resposta.textContent = `${porcentagem}% de ${valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} é ${resultado.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}. A soma do valor mais a porcentagem é ${total.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
     
     // resposta.textContent = `${porcentagem}% de R$${valor.toFixed(2).replace('.',',')} é R$${resultado.toFixed(2).replace('.',',')}. A soma do valor mais a porcentagem é R$${total.toFixed(2).replace('.',',')}`;
 
+});
+
+btnLimpar.addEventListener('click', (e) => {
+    e.preventDefault();
+    elValor.value = '';
+    elPorcentagem.value = '';
+    resposta.textContent = '';
+
+    resposta.style.display = 'none';
+    elValor.focus();
 });
